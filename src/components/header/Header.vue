358490983bac4067ded472ceb4e298f6c57c98a9
Branko Rovcanin <branko.rovcanin@gmail.com>
2020-05-06 15:40:41 +0200

    Started using grid layout css

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -14,12 +14,14 @@
           aria-label="Menu"
           @click="$emit('showCategoryDrawer')"
         />
+      
       <q-avatar class="fixed-right avatar-logo">
         <img src="statics/sports-logo.jpg" />
       </q-avatar>
-      </q-toolbar>
+      <search-products-bar class="search-products-bar"/>
+    </q-toolbar>
       <!-- TODO: Dodati search bar za pretragu svih proizovda -->
-    </q-header>
+  </q-header>
 </template>
 <script>
 export default {
@@ -29,29 +31,31 @@ export default {
     }
   },
   components: {
-    "tool-tip": () => import("../util/ToolTip")
+    "tool-tip": () => import("../util/ToolTip"),
+    "search-products-bar" : () => import("./SearchProducts")
   }
 }
 </script>
 
 <style lang="scss" scoped>
 .q-header {
-  height: 80px;
-  .q-btn {
-    padding: 0px;
-    height: 80px;
-    border-radius: 9px;
-  }
-
-  .q-avatar.avatar-logo {
-  margin-right: 10px;
-  height: 80px;
-  width: 150px;
-
-  .q-img {
-    height: 100%;
-    width: 100%;
+  padding: 0;
+  height: 70px;
+  .q-toolbar {
+    width:100%;
+    height:100%;
+    display: grid;
+    justify-content: space-between;
+    grid-template-areas: '. searchbar searchbar searchbar .';
+    grid-gap: 10px;
+    .search-products-bar {
+      grid-area: searchbar;
+    }
   }
 }
+.q-avatar.avatar-logo {
+  margin-right: 6px;
+  height: 70px;
+  width: 150px;
 }
 </style>
